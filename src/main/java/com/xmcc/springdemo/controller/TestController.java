233c06eb1e18d3a180dc28b82819873b1586f3cb
xmcc <xmcc@163.com>
2019-04-01 16:03:02 +0800

    第一次测试行提交

---
+++ src/main/java/com/xmcc/springdemo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.xmcc.springdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+//    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/hello2")
+    public String hello(){
+//        logger.error("error");
+//        logger.info("info");
+//        logger.debug("debug");
+        log.info("msg {}","hello spring boot");
+        return "hello spring boot 2";
+    }
+}
